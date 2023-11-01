5b02f946a78ad978f026518a269fe1c4c2f1cf7b
sizheng3 <sizheng3@fa23-cs425-1801.cs.illinois.edu>
2023-11-01 17:16:33 -0500

    bugfix

--- internals/SDFS/client.go
+++ internals/SDFS/client.go
@@ -81,6 +81,7 @@ func handleGetFile(sdfsFileName string, localFileName string) {
 		for _, r := range replicas {
 			fmt.Printf("Trying to get file %s from replica: %s\n", sdfsFileName, r)
 			remotePath := getScpHostNameFromHostName(r) + ":" + filepath.Join(SDFS_PATH, sdfsFileName)
+			//limited the speed to 30MB/s
 			cmd := exec.Command("scp", "-l", "240000", remotePath, localFileName)
 			err := cmd.Start()
 			if err != nil {
@@ -257,7 +258,8 @@ func transferFilesConcurrent(localFileName string, sdfsFileName string, targetRe
 func transferFileToReplica(localFileName string, sdfsFileName string, replica string) error {
 	targetHostName := getScpHostNameFromHostName(replica)
 	remotePath := targetHostName + ":" + filepath.Join(SDFS_PATH, sdfsFileName)
-	cmd := exec.Command("scp", "-l", "240000", remotePath, localFileName)
+	//limited the speed to 30MB/s
+	cmd := exec.Command("scp", "-l", "240000", localFileName, remotePath)
 	err := cmd.Start()
 	if err != nil {
 		fmt.Printf("Failed to start command: %v\n", err)
