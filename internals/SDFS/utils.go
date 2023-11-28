570b9b70b5d5615523e626188d21bb5aa51ef970
jyuan19 <jyuan19@illinois.edu>
2023-11-27 22:40:16 -0600

    tune leaderelection timeout

--- internals/SDFS/utils.go
+++ internals/SDFS/utils.go
@@ -144,7 +144,7 @@ func getServerName(id int) string {
 // Generate random duration bewteen 4-6 seconds
 func randomDuration() time.Duration {
 	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(2) + 5
+	n := rand.Intn(3) + 4
 	return time.Duration(n) * time.Second
 }
 
