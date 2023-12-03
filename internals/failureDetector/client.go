5388d77da9860f59e11ad91ae50cafcbd5a8ef42
jyuan19 <jyuan19@illinois.edu>
2023-12-02 21:20:14 -0600

    debug

--- internals/failureDetector/client.go
+++ internals/failureDetector/client.go
@@ -159,7 +159,7 @@ func sendGossipToNodes(selectedNodes []*Node, gossip []byte) {
 		}(addPortNumberToNodeAddr(node.NodeAddr))
 	}
 	wg.Wait()
-	fmt.Printf("Sent group gossip.\n")
+	// fmt.Printf("Sent group gossip.\n")
 }
 
 // Join compute cluster without using an Introducer node. Need all machine's address hardcoded in global.go
